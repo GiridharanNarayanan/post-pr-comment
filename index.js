d8fe9c4fbd705f9b7377c284eef0e7e54479c912
Giridharan Narayanan <ginara@microsoft.com>
2019-11-14 14:43:13 -0800

    123

--- index.js
+++ index.js
@@ -6,22 +6,13 @@ async function run() {
         const token = core.getInput('github_token', { required: true });
 
         const client = new github.GitHub(token);
-        //console.log(`payload: ${JSON.stringify(github.context.payload)}.`);
-
         const pullRequestNumber = github.context.payload.number;
         const pullRequest = github.context.payload.pull_request;
-        //console.log(`pullrequest: ${JSON.stringify(pullRequest)}.`);
-
-        const repo = pullRequest.base.repo.name;
-        //console.log(`repo: ${repo}.`);
-
-        const owner = pullRequest.base.repo.owner;
-        //console.log(`owner: ${owner.login}.`);
-        
+                
         const commentBody = "comment body";
         const response = await client.issues.createComment({
-            owner: owner.login,
-            repo: repo,
+            owner: pullRequest.base.repo.owner.login,
+            repo: pullRequest.base.repo.name,
             issue_number: pullRequestNumber,
             body: commentBody
         });
