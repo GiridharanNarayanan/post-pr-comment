8750f0308c3398fea76d3a4788f3b4fdefcd4e48
Giridharan Narayanan <ginara@microsoft.com>
2019-11-14 14:24:47 -0800

    123

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ async function run() {
 
         const pullRequestNumber = github.context.payload.number;
         const pullRequest = github.context.payload.pull_request;
-        console.log(`pullrequest: ${JSON.stringify(pullRequest)}.`);
+        //console.log(`pullrequest: ${JSON.stringify(pullRequest)}.`);
 
         const repo = pullRequest.base.repo.name;
-        console.log(`repo: ${repo}.`);
+        //console.log(`repo: ${repo}.`);
 
         const owner = pullRequest.base.repo.owner;
-        console.log(`owner: ${owner.login}.`);
+        //console.log(`owner: ${owner.login}.`);
         
         const commentBody = "comment body";
         const response = await client.issues.createComment({
-            owner,
-            repo,
-            pullRequestNumber,
-            commentBody
+            owner: owner.login,
+            repo: repo,
+            issue_number: pullRequestNumber,
+            body: commentBody
         });
 
         console.log(`This is the given PR: ${pullRequestNumber}.`);
